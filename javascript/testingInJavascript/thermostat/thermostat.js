1512d9ae4e1e920a479d001fc0fede6fc0a9ee15
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-10-19 15:41:28 +0100

    Create thermostat class and include first methods getTemperature and up

--- javascript/testingInJavascript/thermostat/thermostat.js
+++ javascript/testingInJavascript/thermostat/thermostat.js
@@ -1,3 +1,15 @@
-class Thermostat {}
+class Thermostat {
+  constructor() {
+    this.temperature = 20;
+  }
+
+  getTemperature() {
+    return this.temperature;
+  }
+
+  up() {
+    this.temperature += 1;
+  }
+}
 
 module.exports = Thermostat;
